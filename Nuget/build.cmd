4ec7a71c700d6082fac4f9e5d0073284bb37e521
ricarod_jar <ricarod_jar@outlook.com>
2020-01-30 20:02:29 +0800

    new version

--- Nuget/build.cmd
+++ Nuget/build.cmd
@@ -1,6 +1,6 @@
-set locationVersion=7.1.6.1
-set 2dMapVersion=1.3.2
-set 3dMapVersion=4.2.3
+set locationVersion=7.2.4 
+set oneVersion=1.3.4.1 
+set twoVersion=4.3.1 
 
 msbuild  ../TencentLocationSdk/TencentLocationSdk.csproj /t:Rebuild /p:Configuration=Release
 msbuild  ../TencentMapSDK_Android_3D/TencentMapSDK_Android_3D.csproj /t:Rebuild /p:Configuration=Release
@@ -8,5 +8,5 @@ msbuild  ../TencentMapSDK_Android_2D/TencentMapSDK_Android_2D.csproj /t:Rebuild 
 
 
 nuget pack tencentlocation.xamarin.android.nuspec -Version %locationVersion%
-nuget pack tencent2dmap.xamarin.android.nuspec -Version %2dMapVersion%
-nuget pack tencent3dmap.xamarin.android.nuspec -Version %3dMapVersion%
+nuget pack tencent2dmap.xamarin.android.nuspec -Version %oneVersion%
+nuget pack tencent3dmap.xamarin.android.nuspec -Version %twoVersion%
---
+++ TencentMapSDK_Android_3D/Additions/TencentMap.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace Com.Tencent.Tencentmap.Mapsdk.Maps
+{
+    public partial class TencentMap : global::Com.Tencent.Tencentmap.Mapsdk.Maps.BaseMap, global::Com.Tencent.Map.Core.Interfaces.IMap
+    {
+        public override void SetMapStyle(int p0)
+        {
+            this.MapStyle = p0;
+        }
+    }
+}
